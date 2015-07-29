d92dd81374b4877ab4c3fd88bb09cae3d9f87509
unknown <idealcoder@idealcoder.co.uk>
2015-07-29 10:24:11 +0100

    Added android build platform

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,33 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.background-mode/www/background-mode.js",
+        "id": "de.appplant.cordova.plugin.background-mode.BackgroundMode",
+        "clobbers": [
+            "cordova.plugins.backgroundMode",
+            "plugin.backgroundMode"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.0",
+    "cordova-plugin-geolocation": "1.0.1",
+    "de.appplant.cordova.plugin.background-mode": "0.6.3",
+    "cordova-plugin-device": "1.0.2-dev"
+}
+// BOTTOM OF METADATA
+});
